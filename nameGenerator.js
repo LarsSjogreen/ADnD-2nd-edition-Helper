bf34bd09a1f0c98ec40826157b796de8e41a7bd8
eliassjogreen <eliassjogreen1@gmail.com>
2017-07-21 15:05:43 +0200

    Made the logo prittier by adding font and updated css

--- nameGenerator.js
+++ nameGenerator.js
@@ -1,17 +1,19 @@
 var mathHelp = require('./mathHelp');
 var nameGenerator = (function() {
-  var firstNameFirst = [ "La", "Me", "Fi", "Ra", "Ber", "I", "Jee", "Ali", "Fru", "Mar", "Na", "El", "Nu", "Gal", "Sul-ba-"];
-  var firstNameSecond = [ "di", "ro", "xu", "ka", "hama", "chibero", "do", "lo", "kalo", "lia"]
+  var firstNameFirst = [ "La", "Me", "Fi", "Ra", "Ber", "I", "Jee", "Ali", "Fru", "Mar", "Na", "El", "Nu", "Gal", "Sul-ba-", "Le"];
+  var firstNameSecond = [ "di", "ro", "xu", "ka", "hama", "chibero", "do", "lo", "kalo", "lia", "roy"]
   var firstNameLast = ["ros", "goa", "xin", "roo", "datch", "lupp", "herobo", "-appo", "s", "tt", "tret"];
-  var places = ["Hemboo", "Wathrobia", "Galubaria", "the Burning forest", "Waterdeep", "Axiria", "under the sea", "Gamorora", "Felexx"];
-  var extraStrong = ["very", "somewhat", "incredibly", "not so", "unsuccessful and", "quite", "sometimes"];
+  var places = ["Hemboo", "Wathrobia", "Galubaria", "the Burning forest", "Waterdeep", "Axiria", "under the sea", "Gamorora", "Felexx", "the Blood Sea", "Icereach", "the Stone city", "Winterheim", "the Dragon Isles"];
+  var extraStrong = ["very", "somewhat", "incredibly", "not so", "unsuccessful and", "quite", "sometimes", "stupidly"];
   var prop = ["brutal", "strong", "angry", "embarrasing", "slippery", "free", "hairy", "fat", "lying bastard", "funny", "boring"];
 
   var generateName = function() {
         var name = firstNameFirst[mathHelp.randomInt(0, firstNameFirst.length)] + firstNameSecond[mathHelp.randomInt(0, firstNameSecond.length)] + firstNameLast[mathHelp.randomInt(0, firstNameLast.length)];
         var add = mathHelp.randomInt(0, 10);
         if (add === 2) {
-          name = name + " from " + places[mathHelp.randomInt(0,places.length)];
+            name = name + " from " + places[mathHelp.randomInt(0,places.length)];
+        } else if (add === 3) {
+            name = name + " of " + places[mathHelp.randomInt(0,places.length)];
         } else if (add > 7) {
           name = name + " the ";
           if (add > 8) {
