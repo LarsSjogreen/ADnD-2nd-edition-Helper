9ac82067197af8e4a4be8335f0b62a9a57cf5b55
Lars Sjögreen <lars.sjogreen@akvo.org>
2017-07-23 19:44:40 +0200

    Making the readTableForAttribute synchronous.

--- tableReader.js
+++ tableReader.js
@@ -7,26 +7,25 @@ var tableReader = (function() {
   var firstRow = true;
 
   var readTableForAttribute = function(stats, filename) {
-    fs.createReadStream(filename).pipe(parse({ delimiter: ','})
-    .on('data', function(line) {
-        if (firstRow) {
-          attribute = line[0];
-          attributeAdjustments = line.slice(1);
-          firstRow = false;
-        } else {
-          doTableRowWork(stats, line, attribute, attributeAdjustments);
-        }
-      })
-    .on('end', function() {
-      console.log(stats);
-      return stats;
-    }));
+    var firstRow = true;
+    var csvFile = fs.readFileSync(filename,'utf8');
+    csvFile.split('\n').forEach(function (line) {
+      var line = line.split(',');
+      if (firstRow) {
+        attribute = line[0];
+        attributeAdjustments = line.slice(1);
+        firstRow = false;
+      } else {
+        doTableRowWork(stats, line, attribute, attributeAdjustments);
+      }
+    });
+    return stats;
   }
 
   var doTableRowWork = function(stats, line, attribute, attributeAdjustments) {
     if (stats[attribute] == line[0]) {
           for (var i =0;i<attributeAdjustments.length;i++) {
-            stats[attributeAdjustments[i]] = line[i+1];
+            stats[attributeAdjustments[i].trim()] = line[i+1].trim();
           }
     }
     return stats;
